4cfdd1705459f8ff584fb2c07be9f5ff815af508
david <david.tagirov.03@gmail.com>
2025-07-01 23:28:00 +0300

    Внедрил фронтенд

--- src/main/java/com/file/storage/repository/UserRepository.java
+++ src/main/java/com/file/storage/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.file.storage.repository;
 
 import com.file.storage.model.User;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,13 +9,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
-
-    Optional<User> findByPassword(String password);
-
-    User findByUsernameAndPassword(
-            @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters") String username,
-            @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters") String password
-    );
-
-    boolean existUserByUsernameAndPassword(String username, String password);
 }
